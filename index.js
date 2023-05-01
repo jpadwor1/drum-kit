cee2157b42c45854fe28b400ec7763dca500250c
John Padworski <128070765+jpadwor1@users.noreply.github.com>
2023-05-01 08:56:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,37 +15,37 @@ function handleClick (key) {
     let audio = "";
         switch (key) {
         case "w":
-            audio = new Audio("/sounds/tom-4.mp3");
+            audio = new Audio("sounds/tom-4.mp3");
             audio.play();
             break;
         
         case "a":
-            audio = new Audio ("/sounds/tom-3.mp3") ;
+            audio = new Audio ("sounds/tom-3.mp3") ;
             audio.play();
             break;
 
         case "s":
-            audio = new Audio("/sounds/tom-2.mp3");
+            audio = new Audio("sounds/tom-2.mp3");
             audio.play();
             break;
 
         case "d":
-            audio = new Audio("/sounds/tom-1.mp3");
+            audio = new Audio("sounds/tom-1.mp3");
             audio.play();
             break;
 
         case "j":
-            audio = new Audio("/sounds/snare.mp3");
+            audio = new Audio("sounds/snare.mp3");
             audio.play();
             break;
 
         case "k":
-            audio = new Audio("/sounds/crash.mp3");
+            audio = new Audio("sounds/crash.mp3");
             audio.play();
             break;
 
         case "l":
-            audio = new Audio("/sounds/kick-bass.mp3");
+            audio = new Audio("sounds/kick-bass.mp3");
             audio.play();
             break;
         default:
